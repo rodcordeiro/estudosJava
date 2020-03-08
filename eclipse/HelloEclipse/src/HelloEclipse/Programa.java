cd73a6adb2730e1c753cb8430bb3d54a61a61222
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-03-08 16:49:24 -0300

    Update files

---
+++ eclipse/HelloEclipse/src/HelloEclipse/Programa.java
@@ -0,0 +1,7 @@
+package HelloEclipse;
+
+public class Programa {
+	public static void main(String[] args) {
+		System.out.println("Hello fucking Universe by Eclipse!!!");
+	}
+}
---
+++ eclipse/HelloEclipse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cordeiro
+ *
+ */
+module HelloEclipse {
+}
